1586042bcf90a44604a808684c59720352ca4551
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-08-21 03:11:39 +0900

    날짜 추가

--- js/clock.js
+++ js/clock.js
@@ -1,12 +1,24 @@
 const clock = document.querySelector("#clock");
+const status = document.querySelector(".clock-status");
+const carendar = document.querySelector(".carendar");
 
 function getClock() {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
+  const years = String(date.getFullYear());
+  const months = String(date.getMonth() + 1).padStart(2, "0");
+  const days = String(date.getDate()).padStart(2, "0");
+
+  if (hours > 12) {
+    status.innerText = "PM";
+  } else {
+    status.innerText = "AM";
+  }
 
   clock.innerText = `${hours}:${minutes}:${seconds}`;
+  carendar.innerText = `${years}년 ${months}월 ${days}일`;
 }
 
 getClock();
